a037faa942b5b6597d48ea05a7a32dd12c4cbea8
VenkataRamanan <113666215+ragulVenkat@users.noreply.github.com>
2024-07-11 14:39:52 +0530

    Add files via upload
    Q4.Write a program that takes a string and returns the number of unique characters in it

---
+++ Unique char.py
@@ -0,0 +1,8 @@
+
+string = input("Enter the String : ")
+
+unique_characters = set(string)
+
+num_unique_characters = len(unique_characters)
+
+print("The number of unique characters in string : ",num_unique_characters)
