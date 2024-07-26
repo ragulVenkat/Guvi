c65d2a902f19fd614b51eb84ecdc7a114dbdae2f
VenkataRamanan <113666215+ragulVenkat@users.noreply.github.com>
2024-07-26 19:31:36 +0530

    Task 4
    Q1. 1. Create a program class called circle with constructor which will take a list as an argument for the task.
    The List is[10,501,22,37,100,999,87,351]

---
+++ Circle.py
@@ -0,0 +1,27 @@
+"""
+1. Create a program class called circle with constructor which will take a list as an argument for the task.
+The List is[10,501,22,37,100,999,87,351]
+
+"""
+
+class Circle:
+
+    num = [10, 501, 22, 37, 100, 999, 87, 351]
+     
+
+    # Constructor 
+    def inte(self):
+        self.num = Circle.num
+    def read(self):
+        print(self.num) 
+
+
+#Object --> venkat
+venkat = Circle()   
+
+print (venkat.num)
+
+venkat.read()
+
+
+        
